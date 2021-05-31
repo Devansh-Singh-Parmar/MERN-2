617802bb200925652c464fc019d62647ae3e6cd9
Devansh Singh Parmar <devansh18dp@gmail.com>
2021-05-31 12:00:23 +0530

    Bug Fixes and a lot of stuff!

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express')
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+
+// DB Connection
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -13,8 +19,19 @@ mongoose.connect(process.env.DATABASE, {
     console.log("DB CONNECTED")
 });
 
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+// My Routes
+app.use("/api", authRoutes);
+
+// PORT
 const port = process.env.PORT || 8000;
 
+// Starting a server
 app.listen(port, () => {
     console.log(`App is running at ${port}`);
 })
